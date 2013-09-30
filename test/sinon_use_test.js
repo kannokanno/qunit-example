5338766ca27c4ec8d2d7cedbb7dbdeca065303e5
m-kanno <akapanna@gmail.com>
2013-09-30 19:09:45 +0900

    Add SinonJS

---
+++ test/sinon_use_test.js
@@ -0,0 +1,14 @@
+var obj = {
+  m: function() { return 10; }
+}
+
+pavlov.specify('Spy(sinonjs)', function(){
+  describe('mock', function(){
+    it('オブジェクト指定してmockにできる', function() {
+      var mock = sinon.mock(obj);
+      mock.expects('m').once().returns(100);
+      assert(obj.m()).equals(100);
+      mock.verify();
+    });
+  });
+});
